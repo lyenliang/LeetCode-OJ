5fe65ddd6d63868234eaa368836ff7f56f25a249
lyenliang <lyenliang@gmail.com>
2015-08-02 17:47:27 +0800

    Remove Duplicates from Sorted Array

--- src/com/solutions/RemoveDuplicatesfromSortedArray.java
+++ src/com/solutions/RemoveDuplicatesfromSortedArray.java
@@ -1,8 +1,5 @@
 package com.solutions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author lyenliang
  *
@@ -11,20 +8,24 @@ public class RemoveDuplicatesfromSortedArray {
 	public int removeDuplicates(int[] nums) {
         int length = nums.length;
         if(length == 0 || length == 1) return length;
-		int p1 = 0;
-		int p2 = 1;
-        int lenCount = 1;
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(nums[0]);
-		for(; p2 < nums.length ; ++p1, ++p2) {
-        	if(nums[p1] != nums[p2]) {
-        		++ lenCount;
-        		list.add(nums[p2]);
-        	}
+        int i=0, j=1;
+        while(j < length){
+            if(nums[i] == nums[j]) {
+            	// go until a different value is found
+            	++j;
+            } else {
+                ++i; // move i to the number to be replaced 
+                nums[i] =nums[j];
+                ++j;
+            }
         }
-		for(int i = 0 ; i < list.size() ; ++i) {
-			nums[i] = list.get(i);
-		}
-		return lenCount;
+        return ++i;
     }
+	
+	public static void main(String[] args) {
+		RemoveDuplicatesfromSortedArray rdsa = new RemoveDuplicatesfromSortedArray();
+		int[] nums = {1,1,1,2,3,3};
+		int length = rdsa.removeDuplicates(nums);
+		System.out.println(length);
+	}
 }

d8b0a74060c923256ca3df94aca8b9f32056762f
lyenliang <lyenliang@gmail.com>
2015-08-08 20:52:33 +0800

    "3 sum" improved by avoiding checking duplicate solutions.

--- src/com/solutions/ThreeSum.java
+++ src/com/solutions/ThreeSum.java
@@ -6,44 +6,39 @@ import java.util.List;
 
 public class ThreeSum {
 	public List<List<Integer>> threeSum(int[] nums) {
-		int length = nums.length;
-		List<List<Integer>> solutions = new ArrayList<List<Integer>>();
-		if(length < 3) return solutions;
-        Arrays.sort(nums);
-        for(int i = 0 ; i < length ; ++i) {
-        	int j = i+1;
-        	int k = length - 1;
-        	while(j < k ) {
-        		int sum = nums[i] + nums[j] + nums[k];
-        		if(sum < 0) {
-    				++j;
-        		} else if(sum > 0) {
-    				--k;
-        		} else {
-        			List<Integer> solution = new ArrayList<Integer>();
-        			solution.add(nums[i]);
-        			solution.add(nums[j]);
-        			solution.add(nums[k]);
-        			if(!solutions.contains(solution)) {
-        				solutions.add(solution);
-        			}
-    				++j;
-    				--k;
-    				while(j < k && nums[j] == nums[j-1]){
-                        ++j;
-                    }
-                    while(j < k && nums[k] == nums[k+1]){
-                        --k;
-                    }
-        		}
-        	}
-        }
-        return solutions;
-    }
+	    List<List<Integer>> solutions = new ArrayList<List<Integer>>();
+	    Arrays.sort(nums);
+	    int i = 0, last = nums.length - 1;
+	    while (i < last) {
+	        int a = nums[i], j = i+1, k = last;
+	        while (j < k) {
+	            int b = nums[j];
+	            int c = nums[k];
+	            int sum = a+b+c;
+	            if (sum == 0) {
+	            	solutions.add(Arrays.asList(a, b, c));
+	            }
+	            if (sum <= 0) {
+	            	// sum == 0 also goes here
+	            	while (nums[j] == b && j < k) { 
+	            		++j;
+	            	} 
+	            }
+	            if (sum >= 0) {
+	            	// sum == 0 also goes here
+	            	while (nums[k] == c && j < k) {
+	            		--k;
+	            	}
+	            }
+	        }
+	        while (nums[i] == a && i < last) ++i;
+	    }
+	    return solutions;
+	}
 	
 	public static void main(String[] args) {
 		ThreeSum ts = new ThreeSum();
-		int[] nums = new int[]{-2,0,1,1,2};
+		int[] nums = new int[]{0,0,0};
 		ts.threeSum(nums);
 	}
 }
